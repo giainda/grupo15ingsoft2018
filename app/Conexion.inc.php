c3aa6f2e8a675d6c3094762ecf6ca12b461c74b4
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-05-10 15:07:44 -0300

    2
    arreglo

--- app/Conexion.inc.php
+++ app/Conexion.inc.php
@@ -6,6 +6,7 @@ class Conexion{
     public static function abrir_conexion(){
         if (!isset(self::$conexion)){
             try{
+                include_once 'config.inc.php';
                 self::$conexion = new PDO('mysql:host='.NOMBRE_SERVIDOR.'; dbname='.NOMBRE_BD,NOMBRE_USUARIO,PASSWORD);
                 self::$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$conexion -> exec("SET CHARACTER SET utf8");
