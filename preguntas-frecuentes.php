d331e39ab0baf69cfc79b22b12d88dc8c992370d
Emanuel Gialleonardo Indart <manugiainda@gmail.com>
2018-07-19 12:06:38 -0300

    19/7 preguntas frecuentes posible final
    Signed-off-by: Emanuel Gialleonardo Indart <manugiainda@gmail.com>

--- preguntas-frecuentes.php
+++ preguntas-frecuentes.php
@@ -9,57 +9,67 @@ include_once "plantillas/navbar2.inc.php";
    <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">Que es unAventon?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>UnAventon es una plataforma donde usuarios de todo el país pueden acordar para compartir gastos de viaje</h4>
                         </div>
                         </div>
                         <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">Como funciona unAventon?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>Es simple, una persona que tiene planeado un viaje y tiene espacios vacíos en su vehiculo, lo publica y los interesados en sumarse a ese viaje mandan su solicitud para que este los acepte.</h4>
                         </div>
                         </div>
                         <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">Que hace falta para publicar un viaje?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>Para publicar un viaje necesitas registrarte a la pagina, registrar un vehículo y tener una cuenta bancaria asociada a tu usuario, nada mas!</h4>
                         </div>
                         </div>
                         <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">El uso de la plataforma es gratuito?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>No, del total del precio del viaje, la pagina cobra una comisión del 5%</h4>
                         </div>
                         </div>
                         <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">Como sé si un usuario es de confianza?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>La pagina posee un sistema de reputación, donde los usuarios obtienen calificación positiva o negativa dependiendo de que tan bien o mal hayan sido evaluados por sus compañeros de viajes anteriores</h4>
                         </div>
                         </div>
                         <br>
    <div class="card rounded-0">
                         <div class="card-header">
-                            <h3 class="mb-0">Pregunta</h3>
+                            <h3 class="mb-0">Que sucede en caso de que haya publicado un viaje y luego me arrepienta o no pueda asistir?</h3>
                         </div>
                         <div class="card-body">
-                         <h3>Respuesta</h4>
+                         <h3>En caso de que no existan personas ya aceptadas en tu viaje, no pasa nada, existe una opción para eliminar el viaje, caso contrario, se te penalizara por cada uno de ellas con una calificación negativa</h4>
                         </div>
-                        </div>                                                                                                         
+                        </div> 
+                        <br>  
+                        <div class="card rounded-0">
+                        <div class="card-header">
+                            <h3 class="mb-0">como funciona el sistema de pagos de la pagina?</h3>
+                        </div>
+                        <div class="card-body">
+                         <h3>Se puede pagar mediante dos métodos, con dinero de tu cuenta, o mediante uso directo de la tarjeta, no obstante, se deberá tener una tarjeta asociada si o si, ya que de otra manera no se podrá acceder a las funciones de la plataforma unAventon.</h4>
+                        </div>
+                        </div> 
+                        <br>                                                                                                                             
                          
 </div>
 
